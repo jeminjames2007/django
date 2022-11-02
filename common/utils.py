657199aa4901eb5caf870569eaf4032340e76bed
Jemin <jemin@focaloid.com>
2022-11-02 14:27:33 +0530

    Functions

--- common/utils.py
+++ common/utils.py
@@ -4,17 +4,12 @@ import os
 from django.utils.crypto import get_random_string
 
 
-class RenameUpload(object):
-
-    def __init__(self, sub_path):
-        self.path = sub_path
-
-    def __call__(self, instance, filename):
+def rename_upload(path):
+    def wrapper(instance, filename):
+        ext = filename.split(".")[-1]
         unique_id = str(get_random_string(length=32))
-        ext = filename.split('.')[-1]
         if instance.pk:
-            filename = '{}{}.{}'.format(instance.pk,unique_id, ext)
+            filename = "{}{}.{}".format(instance.pk, unique_id, ext)
         else:
-            filename = '{}{}.{}'.format(uuid4().hex,unique_id, ext)
-        return os.path.join(self.path, filename)
-
+            filename = "{}{}.{}".format(uuid4().hex, unique_id, ext)
+        return os.path.join(path, filename)
--- products/models.py
+++ products/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from catagory.models import Catagory
 from django.utils.safestring import mark_safe
-from django.utils.crypto import get_random_string
-from common.utils import RenameUpload
+from common.utils import *
+
 
 class Products(models.Model):
+
     name = models.CharField(max_length=100)
-    category = models.ForeignKey(Catagory,on_delete=models.CASCADE,default=False,null=False)
-    image = models.ImageField(upload_to = RenameUpload('product_images/'),default='',null=False)
-    quantity = models.IntegerField(default=False,null=False)
-    price = models.IntegerField(default=False,null=False)
+    category = models.ForeignKey(
+        Catagory, on_delete=models.CASCADE, default=False, null=False
+    )
+    image = models.FileField(
+        upload_to=rename_upload("product_images/"), default=None, null=False
+    )
+    quantity = models.IntegerField(default=False, null=False)
+    price = models.IntegerField(default=False, null=False)
 
     def product_image(self):
         return mark_safe('<img src="{}" width = "100" />'.format(self.image.url))
-
-
-
